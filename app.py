f02fe586ac54a91d205d4105db54eef58e3979e3
Tanisha <tanisha.paramanik@gmail.com>
2025-07-15 20:35:11 +0530

    Basic registration and login page

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template("index.html")
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        return "Registered Successfully"
+    return render_template("register.html")
+
+@app.route('/register', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        return "Logged In"
+    return render_template("login.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
